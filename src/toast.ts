c574c6147ccd78ff24ffeaf5387d20b6090c474e
alphahm <ahmwebdev@gmail.com>
2022-10-17 08:55:25 +0100

    use js encapsulation whenever possible

--- src/toast.ts
+++ src/toast.ts
@@ -9,28 +9,28 @@ enum NotificationType {
 }
 
 class Toast {
-    private alerts: Array<INotification>;
-    private isBusy: boolean;
-    private intervalId: number;
-    private body: HTMLBodyElement
+    #alerts: Array<INotification>;
+    #isBusy: boolean;
+    #intervalId: number;
+    #body: HTMLBodyElement
 
-    private static _instance: Toast;
+    static #instance: Toast;
 
     private constructor() {
         // this is necessary for when the class is called in a context where
         // a private constructor is not enforced
-        if(Toast._instance) {
+        if(Toast.#instance) {
             throw new Error("Error: Instantiation failed: Use Toast.Instance.");
         }
-        Toast._instance = this;
-        this.alerts = [];
-        this.isBusy = false;
-        this.intervalId = 0;
-        this.body = document.querySelector("body")!;
+        Toast.#instance = this;
+        this.#alerts = [];
+        this.#isBusy = false;
+        this.#intervalId = 0;
+        this.#body = document.querySelector("body")!;
     }
    
     public static get Instance(): Toast {
-        return Toast._instance ?? (Toast._instance = new Toast());
+        return Toast.#instance ?? (Toast.#instance = new Toast());
     }
 
     public notify(alertType: NotificationType, message: string): void {
@@ -39,40 +39,40 @@ class Toast {
             message: message
         }
 
-        this.alerts.push(notification);
-        this.handleQueue();
+        this.#alerts.push(notification);
+        this.#handleQueue();
     }
 
-    private handleQueue(): void {
+    #handleQueue(): void {
         // if there's already an interval, we don't need to create a new one
         // because we're already checking for new elements in the alerts queue
-        if (this.intervalId) {
+        if (this.#intervalId) {
             return;
         }
 
         // if we're already showing a notification, we have to wait
-        if (this.isBusy) {
-            this.intervalId = setInterval(() => {
-                if (this.alerts.length > 0 && !this.isBusy) {
-                    this.createAlertDiv();
+        if (this.#isBusy) {
+            this.#intervalId = setInterval(() => {
+                if (this.#alerts.length > 0 && !this.#isBusy) {
+                    this.#createAlertDiv();
                 }
                 
                 // no more alerts in the queue, can clear the interval
-                if (this.alerts.length === 0 && !this.isBusy) {
-                    clearInterval(this.intervalId);
-                    this.intervalId = 0;
+                if (this.#alerts.length === 0 && !this.#isBusy) {
+                    clearInterval(this.#intervalId);
+                    this.#intervalId = 0;
                 }
             }, 500);
         }
         else {
-            this.createAlertDiv();
+            this.#createAlertDiv();
         }
     }
 
-    private createAlertDiv(): void {
-        this.isBusy = true;
+    #createAlertDiv(): void {
+        this.#isBusy = true;
 
-        const notification_obj = this.alerts.shift()
+        const notification_obj = this.#alerts.shift()
 
         if (notification_obj == undefined) {
             return;
@@ -100,21 +100,21 @@ class Toast {
         toastContent.appendChild(toastContentMsg);
         toastDiv.appendChild(toastContent);
 
-        this.body.appendChild(toastDiv);
-        this.destroy(toastDiv);
+        this.#body.appendChild(toastDiv);
+        this.#destroy(toastDiv);
     }
     
-    private destroy(div: HTMLDivElement): void {
+    #destroy(div: HTMLDivElement): void {
         div.classList.remove("show-toast");
         div.classList.add("hide-toast");
 
         setTimeout(() => {
-            this.body.removeChild(div);
+            this.#body.removeChild(div);
         }, 1500);
 
         // wait a bit to avoid rapid firing of notifications
         setTimeout(() => {
-            this.isBusy = false;
+            this.#isBusy = false;
         }, 2000);
     }
 }
